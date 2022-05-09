099154bc1f23e2aaf45e402cd56d2b6ec7d781ce
Surendra1000 <105191202+Surendra1000@users.noreply.github.com>
2022-05-09 21:10:20 +0530

    CSE326 PROJECT

---
+++ CSE326 CA1 CODES/JavaScript/logic.js
@@ -0,0 +1,11 @@
+function fun1(){
+    alert("Basic Pack is Successfully Added To Your Cart");
+}
+
+function fun2(){
+    alert("Pro Pack is Successfully Added To Your Cart");
+}
+
+function fun3(){
+    alert("Advance Pack is Successfully Added To Your Cart");
+}
